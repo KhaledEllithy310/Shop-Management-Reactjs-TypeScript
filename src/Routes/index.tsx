f6d482728bba328c15941715a648caf7bf6f218c
KhaledEllithy <khaledellessy310@gmail.com>
2024-01-14 23:15:39 +0200

    main structure of project

---
+++ src/Routes/index.tsx
@@ -0,0 +1,14 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
+import Home from '../Pages/Home'
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<Home />}></Route>
+    </>
+  )
+)
